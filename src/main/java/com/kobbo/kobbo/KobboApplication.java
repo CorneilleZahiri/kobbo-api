fe2c8bbfc573eed78c116204a191bed41ec74e7b
Corneille <cgbeaga@gmail.com>
2025-07-12 19:05:19 +0100

    Ajout di fichier .env pour les codes secrets

--- src/main/java/com/kobbo/kobbo/KobboApplication.java
+++ src/main/java/com/kobbo/kobbo/KobboApplication.java
@@ -8,7 +8,7 @@ public class KobboApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KobboApplication.class, args);
-		
+		System.out.println("Kobbo started");
 	}
 
 }
