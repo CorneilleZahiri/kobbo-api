9ca6ea979dd60550d8aafd23259534e754a1e536
Corneille <cgbeaga@gmail.com>
2025-07-12 16:51:33 +0100

    Initialisation du projet avec le fichier readme.md

---
+++ src/main/java/com/kobbo/kobbo/KobboApplication.java
@@ -0,0 +1,13 @@
+package com.kobbo.kobbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KobboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KobboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kobbo/kobbo/KobboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kobbo.kobbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KobboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
