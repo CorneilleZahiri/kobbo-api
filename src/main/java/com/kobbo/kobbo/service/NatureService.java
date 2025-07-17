f2f16d6566a0a05a660351211bf79c60b1c97bdc
Corneille <cgbeaga@gmail.com>
2025-07-17 20:30:00 +0100

    Création de nature par société avec contrôle de doublon sur l'intitulé nature

--- src/main/java/com/kobbo/kobbo/service/NatureService.java
+++ src/main/java/com/kobbo/kobbo/service/NatureService.java
@@ -1,20 +1,40 @@
 package com.kobbo.kobbo.service;
 
 import com.kobbo.kobbo.dto.nature.NatureDto;
+import com.kobbo.kobbo.dto.nature.RegisterNatureRequest;
 import com.kobbo.kobbo.entity.Nature;
+import com.kobbo.kobbo.entity.Societe;
+import com.kobbo.kobbo.exception.DuplicateEntryException;
 import com.kobbo.kobbo.mapper.NatureMapper;
 import com.kobbo.kobbo.repository.NatureRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class NatureService {
     private final NatureRepository natureRepository;
     private final NatureMapper natureMapper;
+    private final SocieteService societeService;
 
-    public NatureDto creerNature(Nature nature) {
-        Nature natureSaved = natureRepository.save(nature);
-        return natureMapper.toDto(natureSaved);
+    public NatureDto createNature(RegisterNatureRequest request, UUID societeID) {
+        //Vérifier l'existence de Société
+        Societe societe = societeService.getSocieteById(societeID);
+
+        //Contrôler le doublon sur l'intitulé
+        if (getNatureByIntitule(request.getIntitule(), societe) != null) {
+            throw new DuplicateEntryException("La nature " + request.getIntitule(), societe.getRaisonSociale());
+        }
+
+        Nature nature = natureMapper.toEntity(request);
+        nature.setSociete(societe);
+
+        return natureMapper.toDto(natureRepository.save(nature));
+    }
+
+    public Nature getNatureByIntitule(String intitule, Societe societe) {
+        return natureRepository.findByIntituleAndSociete(intitule, societe).orElse(null);
     }
 }
