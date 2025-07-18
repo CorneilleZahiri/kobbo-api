ca039497891f5956a64dae550b082fd060e0bc25
Corneille <cgbeaga@gmail.com>
2025-07-18 19:55:34 +0100

    Correction de l'analyse: suppression du lien entre utilisateurs et societes, ajout de societe dans profil_utilisateur

--- src/main/resources/database/migration/V2__Add_column_statut_and_unique_constaint.sql
+++ src/main/resources/database/migration/V2__Add_column_statut_and_unique_constaint.sql
@@ -3,3 +3,11 @@ ALTER TABLE natures ADD CONSTRAINT unique_intitule_societe_id UNIQUE (intitule, 
 ALTER TABLE operations ADD COLUMN statut VARCHAR(50) NOT NULL DEFAULT 'VALIDE';
 
 CREATE INDEX index_statut ON operations (statut);
+
+ALTER TABLE utilisateurs DROP FOREIGN KEY fk_utilisateur_societes;
+
+ALTER TABLE utilisateurs DROP COLUMN societes_id;
+
+ALTER TABLE profil_utilisateurs ADD COLUMN societes_id binary(16) NOT NULL;
+
+ALTER TABLE profil_utilisateurs ADD CONSTRAINT fk_societe_profil FOREIGN KEY (societes_id) REFERENCES societes (id) ON DELETE CASCADE;
