e80b124a9457f7a4ae13f44b12714c7c1de7a9b9
Corneille <cgbeaga@gmail.com>
2025-07-25 14:09:03 +0100

    La classe Operation crée avec succès

--- src/main/java/com/kobbo/kobbo/entity/Operation.java
+++ src/main/java/com/kobbo/kobbo/entity/Operation.java
@@ -2,19 +2,60 @@ package com.kobbo.kobbo.entity;
 
 import com.kobbo.kobbo.enumeration.Statut;
 import com.kobbo.kobbo.enumeration.TypeOperation;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Entity
+@Table(name = "operations")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Operation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
     private UUID id;
-    private LocalDate dateOperation;
-    private Statut statut;
-    private TypeOperation typeOperation;
-    private String libelle;
-    private Long montant;
-    private String modeDePayement;
-    private String referenceModeDePayement;
-    
 
+    @Column(name = "date_operation")
+    private LocalDate dateOperation;
+
+    @Column(name = "statut")
+    private Statut statut;
+
+    @Column(name = "type_operation")
+    private TypeOperation typeOperation;
+
+    @Column(name = "libelle")
+    private String libelle;
+
+    @Column(name = "montant")
+    private Long montant;
+
+    @Column(name = "mode_de_payement")
+    private String modeDePayement;
+
+    @Column(name = "reference_mode_de_payement")
+    private String referenceModeDePayement;
+
+    @Column(name = "numero_formater")
+    private String numeroFormater;
+
+    @ManyToOne
+    @JoinColumn(name = "tiers_id")
+    private Tiers tiers;
+
+    @ManyToOne
+    @JoinColumn(name = "natures_id")
+    private Nature nature;
+
+    @ManyToOne
+    @JoinColumn(name = "utilisateurs_id")
+    private Utilisateur utilisateur;
 }
